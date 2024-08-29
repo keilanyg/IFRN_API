81844de169bf4b0631315d09e3ef93e9fc78fa2d
IzadoraFernandes <izadorasilvams@gmail.com>
2024-08-29 14:00:09 -0300

    trabalhando co json-server para cadastro de livros

--- books/urls.py
+++ books/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from .viewsets import *
 from rest_framework import routers
 
+
 router = routers.DefaultRouter()
 app_name = 'books'
 router.register("categoria", CategoriaViewSet)
@@ -12,4 +13,6 @@ router.register("livro", LivroViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    
+
 ]
